fb02ccf431471c7dbc56036ee2f9c5b844ab9be1
llqqssttyy <326ekdms@gmail.com>
2021-07-09 05:52:23 +0900

    플랫폼 구현
    moving platform - 딜레이 타임 추가 예정
    fade platform - 구현 완료

---
+++ Assets/2_Scripts/Platform/FadeOutPlatform.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class FadeOutPlatform : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    
+    private float fadeOutSpeed = 0.1f; //투명도가 0.1깎일 때까지 걸리는 시간, 클수록 느려짐
+    private float delayTime = 0; //layer가 바뀌기 전까지 걸리는 시간
+    private float coolTime = 2f; //없어진 플랫폼이 다시 나타날 때 까지의 쿨타임
+
+    private int cnt = 0;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            cnt++;
+            
+            if(cnt == 1)
+                StartCoroutine(FadeOut());
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        for (float i = 10; i >= 0; i--)
+        {
+            float alpha = i / 10.0f;
+            _spriteRenderer.color = new Color(1,1,1,alpha);
+            
+            if (alpha == 0.7f)
+            {
+                Debug.Log("이거 되는겨?");
+                GetComponent<BoxCollider2D>().isTrigger = true;
+            }
+            yield return new WaitForSeconds(fadeOutSpeed);
+        }
+        
+        yield return new WaitForSeconds(coolTime);
+        
+        _spriteRenderer.color = new Color(1,1,1,1);
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        
+        cnt = 0;
+    }
+}
--- Assets/2_Scripts/Platform/MovingPlatform.cs
+++ Assets/2_Scripts/Platform/MovingPlatform.cs
@@ -1,18 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float goSpeed = 2f;
+    private float backSpeed = 1.5f;
+    private float delayTime;
+    private bool isMoving = false;
+
+    [Header("Positions")]
+    [SerializeField] protected Transform startPos;
+    [SerializeField] protected Transform pos1;
+    [SerializeField] protected Transform pos2;
+    
+    private Vector3 nextPos;
+
+    private void FixedUpdate()
     {
+        SetNextPosition();
         
+        if (isMoving)
+        {
+            MovePlatform();
+        }
+        
+        Debug.Log("isMoving : " + isMoving);
     }
 
-    // Update is called once per frame
-    void Update()
+    void MovePlatform()
     {
+        //목표지점까지 current 지점을 직선이동
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, goSpeed * Time.deltaTime);
         
+        if (gameObject.transform.position == startPos.position)
+        {
+            isMoving = false;
+        }
+    }
+    
+    void SetNextPosition()
+    {
+        if (gameObject.transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        else if (gameObject.transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            isMoving = true;
+        }
     }
 }
