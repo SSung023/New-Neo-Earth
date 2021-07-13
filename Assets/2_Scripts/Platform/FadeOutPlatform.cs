e40d2940db12c60453ef24d335dde77a62c3aeb8
llqqssttyy <326ekdms@gmail.com>
2021-07-13 21:58:25 +0900

    플랫폼 콜라이더 수정
    1. player data -> jump force 6->7로 수정
    2. moving platform, fade out platform 트리거 박스 콜라이더 크기 수정

--- Assets/2_Scripts/Platform/FadeOutPlatform.cs
+++ Assets/2_Scripts/Platform/FadeOutPlatform.cs
@@ -18,7 +18,7 @@ public class FadeOutPlatform : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
