53cc121f72da47277ef199d3202c19379a06a98e
SSung <50323157+Sung2019@users.noreply.github.com>
2021-07-15 01:49:07 +0900

    Falling Platform 기본
    n초 뒤에 목표 지점에 떨어지는 기능

--- Assets/2_Scripts/Platform/FallingPlatform.cs
+++ Assets/2_Scripts/Platform/FallingPlatform.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private PlayerData playerData;
-    
-    
+    [SerializeField] private Transform landingPos;
+    [SerializeField] private Vector2 checkVector;
+    private LayerMask playerLayer;
+    private Transform checkPos;
+
+    private bool isPlayerOn = false; // 플레이어가 플랫폼에 닿아있는가의 여부
+    private bool canExecute; // 플랫폼의 기능이 실행될 수 있는가의 여부
+
+    private int cnt = 0;
+    private const float standbyTime = 1f; // 플랫폼 대기 시간
+
+
+    private void Awake()
+    {
+        playerLayer = LayerMask.GetMask("Player");
+        checkPos = transform.GetChild(0);
+    }
     private void Start()
     {
         
@@ -14,6 +30,34 @@ public class FallingPlatform : MonoBehaviour
     
     private void Update()
     {
+        CheckPlayer();
         
+    }
+
+    private void CheckPlayer()
+    {
+        if (!canExecute)
+        {
+            isPlayerOn = Physics2D.OverlapBox(checkPos.position, checkVector, 0, playerLayer);
+            if (isPlayerOn)
+            {
+                StartCoroutine(StandbyAndDrop());
+                canExecute = true;
+            }
+        }
+    }
+
+    private IEnumerator StandbyAndDrop()
+    {
+        yield return new WaitForSeconds(standbyTime);
+        transform.DOMove(landingPos.position, 0.3f).SetEase(Ease.InQuart);
+    }
+    
+    
+    // Debug
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(checkPos.position, checkVector);
     }
 }
