5bb4c306481a341820bdd92826de76ad4d71fccc
llqqssttyy <326ekdms@gmail.com>
2021-07-15 03:34:33 +0900

    Hologram Field
    중간과정

---
+++ Assets/2_Scripts/Utility/HologramManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramManager : MonoBehaviour
+{
+    private bool dpHologram;
+
+    private GameObject hologramObj;
+
+    private void Start()
+    {
+        dpHologram = false;
+        hologramObj = GameObject.FindWithTag("Hologram On");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log(dpHologram);
+            dpHologram = !dpHologram;
+        }
+        LoadHologram();
+    }
+
+    private void SetMode()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log(dpHologram);
+            dpHologram = !dpHologram;
+        }
+    }
+    
+    private void LoadHologram()
+    {
+        if (dpHologram)
+        {
+            hologramObj.SetActive(true);
+        }
+        else
+        {
+            hologramObj.SetActive(false);
+        }
+    }
+}
