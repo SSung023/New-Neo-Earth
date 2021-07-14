0960e74f371e47ab99bdc9920ad1965a78216824
llqqssttyy <326ekdms@gmail.com>
2021-07-15 00:38:16 +0900

    Moving Platform 수정 완

--- Assets/2_Scripts/Platform/MovingPlatform.cs
+++ Assets/2_Scripts/Platform/MovingPlatform.cs
@@ -8,7 +8,7 @@ public class MovingPlatform : MonoBehaviour
     private float goSpeed = 4f;
     private float backSpeed = 1.5f; //(구현x)갔다가 올 때 속도 다르면 좋겠다고 할 수도 있어서 변수 2개 만듦
     private float delayTime = 1f; //(구현x)발판 밟을 때 살짝 기다리는 시간
-    private bool isMoving = false;
+    private bool canMove = false;
 
     [Header("Positions")]
     [SerializeField] protected Transform startPos;
@@ -20,9 +20,13 @@ public class MovingPlatform : MonoBehaviour
     {
         SetNextPosition();
         
-        if (isMoving)
+        if (canMove)
         {
             StartCoroutine("MovePlatform");
+        }
+        else
+        {
+            StopCoroutine("MovePlatform");
         }
     }
 
@@ -35,7 +39,7 @@ public class MovingPlatform : MonoBehaviour
         
         if (gameObject.transform.position == startPos.position)
         {
-            isMoving = false;
+            canMove = false;
         }
     }
 
@@ -55,7 +59,7 @@ public class MovingPlatform : MonoBehaviour
     {
         if (col.CompareTag("Player"))
         {
-            isMoving = true;
+            canMove = true;
         }
     }
 
@@ -63,7 +67,7 @@ public class MovingPlatform : MonoBehaviour
     {
         if (col.CompareTag("Player") && gameObject.transform.position == startPos.position)
         {
-            isMoving = true;
+            canMove = true;
         }
     }
 
