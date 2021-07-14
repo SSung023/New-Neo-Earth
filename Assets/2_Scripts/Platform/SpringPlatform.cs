204fa8e4d07c5c93feec9d6df9337916a8bc69d0
llqqssttyy <326ekdms@gmail.com>
2021-07-14 17:23:15 +0900

    Sprign Platform

---
+++ Assets/2_Scripts/Platform/SpringPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringPlatform : MonoBehaviour
+{
+    private float jumpForce = 8f;
+    private GameObject playerObj;
+    
+    private void Start()
+    {
+        playerObj = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            playerObj.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
