ac0d607d71086aae9346b92685e9c7632e737053
llqqssttyy <326ekdms@gmail.com>
2021-07-27 16:05:17 +0900

    fadeout platform 수정

--- Assets/2_Scripts/Platform/FadeOutPlatform.cs
+++ Assets/2_Scripts/Platform/FadeOutPlatform.cs
@@ -10,9 +10,10 @@ public class FadeOutPlatform : MonoBehaviour
     private BoxCollider2D boxCollider2D;
     private GameObject wallCheck; // Wall layer가 있는 오브젝트(자식 오브젝트)
     
-    private float fadeOutSpeed = 0.1f; //투명도가 0.1깎일 때까지 걸리는 시간, 클수록 느려짐
+    private float fadeOutDegree = 0.2f; //투명도가 0.1깎일 때까지 걸리는 시간, 클수록 느려짐
     private float delayTime = 0; //layer가 바뀌기 전까지 걸리는 시간
     private float coolTime = 2f; //없어진 플랫폼이 다시 나타날 때 까지의 쿨타임
+    private float disableTiming = 0.7f;
 
     private int cnt = 0;
     private void Awake()
@@ -43,15 +44,15 @@ public class FadeOutPlatform : MonoBehaviour
             spriteRenderer.color = new Color(1,1,1,alpha);
             
             //alpha가 0.7이 되는 순간에 플랫폼 콜라이더 통과로 바꾸기
-            if (alpha == 0.7f)
+            if (alpha == disableTiming)
             {
                 boxCollider2D.isTrigger = true;
                 wallCheck.SetActive(false);
             }
-            yield return new WaitForSeconds(fadeOutSpeed);
+            yield return new WaitForSeconds(fadeOutDegree);
         }
         
-        /*
+        
         yield return new WaitForSeconds(coolTime);
         
         spriteRenderer.color = new Color(1,1,1,1);
@@ -59,6 +60,6 @@ public class FadeOutPlatform : MonoBehaviour
         wallCheck.SetActive(true);
         
         cnt = 0;
-        */
+        
     }
 }
