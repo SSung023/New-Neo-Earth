f1990ae00d6431cf8176efb5b854f31364778bcb
SSung <50323157+Sung2019@users.noreply.github.com>
2021-10-05 17:06:11 +0900

    Laser trap 작업
    1. 작업을 위한 Map1_proto scene 생성
    
    2. LineRenderer를 통한 Laser trap 작업 중
    현재 LineRenderer를 통해 광선이 나오기만함

---
+++ Assets/2_Scripts/Object/LaserBeam.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class LaserBeam
+{
+    private Vector3 pos, dir;
+    private GameObject laserObj;
+    private LineRenderer laserRenderer;
+    private List<Vector3> laserIndex = new List<Vector3>();
+    
+    public LaserBeam(Vector3 pos, Vector3 dir, Material material)
+    {
+        this.laserRenderer = new LineRenderer();
+        this.laserObj = new GameObject();
+        this.laserObj.name = "Laser";
+        this.pos = pos;
+        this.dir = dir;
+        
+        this.laserRenderer = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        this.laserRenderer.startWidth = 0.1f;
+        this.laserRenderer.endWidth = 0.1f;
+        this.laserRenderer.material = material;
+        this.laserRenderer.startColor = Color.green;
+        this.laserRenderer.endColor = Color.green;
+        
+        //tmp code
+        laserIndex.Add(pos);
+        laserIndex.Add(new Vector3(pos.x, pos.y - 10, pos.z));
+        laserRenderer.SetPositions(laserIndex.ToArray());
+        laserRenderer.useWorldSpace = true;
+
+        //CastRay(pos, dir, laserRenderer);
+    }
+    
+
+    void CastRay(Vector3 pos, Vector3 dir, LineRenderer lineRenderer)
+    {
+        laserIndex.Add(pos);
+
+        Ray2D ray = new Ray2D(pos, dir);
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(ray.origin,dir, 50);
+
+        // Ray ray2 = new Ray(pos, dir);
+        // RaycastHit hit;
+        // Physics.Raycast(ray2, out hit, 30, 1);
+
+        if (raycastHit2D.collider != null)
+        {
+            laserIndex.Add(raycastHit2D.point);
+            UpdateLaser();
+        }
+        else
+        {
+            laserIndex.Add(ray.GetPoint(30));
+            UpdateLaser();
+        }
+    }
+
+    void UpdateLaser()
+    {
+        int count = 0;
+        laserRenderer.positionCount = laserIndex.Count;
+
+        foreach (Vector3 idx in laserIndex)
+        {
+            laserRenderer.SetPosition(count, idx);
+            count++;
+        }
+    }
+}
---
+++ Assets/2_Scripts/Object/LaserShooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShooter : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    private LaserBeam laserBeam;
+    
+
+    // private void Update()
+    // {
+    //     Destroy(GameObject.Find("Laser"));
+    //     laserBeam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+    // }
+
+    private void Start()
+    {
+        laserBeam = new LaserBeam(gameObject.transform.position, gameObject.transform.up * -1, material);
+    }
+}
