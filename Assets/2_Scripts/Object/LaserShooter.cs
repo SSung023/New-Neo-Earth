55b3eafabef9003b8f94991286006d63649c9ac9
SSung <50323157+Sung2019@users.noreply.github.com>
2021-10-18 17:15:14 +0900

    laser trap 중간 저장

--- Assets/2_Scripts/Object/LaserShooter.cs
+++ Assets/2_Scripts/Object/LaserShooter.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class LaserShooter : MonoBehaviour
 {
-    [SerializeField] private Material material;
-    private LaserBeam laserBeam;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float rayDistance = 100;
+    [SerializeField] private Transform laserFirePos;
+    private Transform m_transform;
+    private LineRenderer lineRenderer;
+    
     
 
-    // private void Update()
-    // {
-    //     Destroy(GameObject.Find("Laser"));
-    //     laserBeam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
-    // }
-
-    private void Start()
+    private void Awake()
     {
-        laserBeam = new LaserBeam(gameObject.transform.position, gameObject.transform.up * -1, material);
+        m_transform = GetComponent<Transform>();
+        lineRenderer = GetComponent<LineRenderer>();
+        
+        this.lineRenderer.startWidth = 0.1f;
+        this.lineRenderer.endWidth = 0.1f;
+        this.lineRenderer.startColor = Color.green;
+        this.lineRenderer.endColor = Color.green;
+    }
+
+    private void Update()
+    {
+        ShootLaser();
+    }
+
+    private void ShootLaser()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(m_transform.position, transform.up * -1, int.MaxValue,layerMask);
+
+        if (hit.collider != null)
+        {
+            DrawRay2D(m_transform.position, hit.point);
+        }
+        else
+        {
+            DrawRay2D(m_transform.position, m_transform.up * (-1  * rayDistance));
+        }
+    }
+
+    private void DrawRay2D(Vector2 start, Vector2 end)
+    {
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
     }
 }
